caebf2ac6b726a28162bbbd715f51921e9aee8f7
Yuvraj Sharma <yuvraj.june2002@gmail.com>
2024-05-03 10:15:46 +0530

    Create 165-compare-version-numbers.cpp

---
+++ 165-compare-version-numbers.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int compareVersion(string version1, string version2) {
+        int n = version1.size(), m = version2.size();
+        int i = 0, j = 0;
+
+        while (i < n || j < m) {
+            int num1 = 0;
+            while (i < n && version1[i] != '.') {
+                num1 = num1 * 10 + (version1[i] - '0');
+                i++;
+            }
+
+            int num2 = 0;
+            while (j < m && version2[j] != '.') {
+                num2 = num2 * 10 + (version2[j] - '0');
+                j++;
+            }
+
+            if (num1 < num2)
+                return -1;
+            if (num1 > num2)
+                return 1;
+            i++, j++;
+        }
+        return 0;
+    }
+};
